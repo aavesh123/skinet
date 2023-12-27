d179ad6814631f4eec237dfa462dd6b3b1c8d97b
LAPTOP-GG7E7TR0\Avesh <aavesh123@gmail.com>
2023-12-27 10:03:21 +0530

    26. Configuring the migrations

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,5 +14,11 @@ namespace Infrastructue.Data
 
         public DbSet<ProductBrand> ProductBrand { get; set; }
         public DbSet<ProductType> ProductType { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
