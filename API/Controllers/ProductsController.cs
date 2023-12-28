2dd2799bc6a5b63403c919788d2791b7404e7b90
LAPTOP-GG7E7TR0\Avesh <aavesh123@gmail.com>
2023-12-28 17:01:21 +0530

    66. Adding Pagination

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 using API.Dtos;
 using API.Errors;
+using API.Helpers;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
@@ -25,10 +26,18 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort, int? brandId, int? typeId)
+        public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts([FromQuery]ProductSpecParams productParams)
         {
-            var spec = new ProductsWithTypesAndBrandSpecifications(sort, brandId, typeId);
+            var spec = new ProductsWithTypesAndBrandSpecifications(productParams);
+
+            var countSpec = new ProductWithFiltersForCountSpecific(productParams);
+
+            var totalItems = await _productsRepo.CountAsync(countSpec);
+
             var products = await _productsRepo.ListAsync(spec);
+
+            var data = _mapper
+            .Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
 
             // return Ok(products);
             // return products.Select(product => new ProductToReturnDto {
@@ -40,8 +49,7 @@ namespace API.Controllers
             //     ProductBrand = product.ProductBrand.Name,
             //     ProductType = product.ProductType.Name
             // }).ToList();
-            return Ok(_mapper
-            .Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
+            return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex, productParams.PageSize, totalItems, data));
         }
 
         [HttpGet("{id}")]
